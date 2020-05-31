aae50693bb38ba9f1f76f25625638fadbffff8a9
patart19 <62774304+patart19@users.noreply.github.com>
2020-05-31 18:30:12 -0400

    Cleaned up some small formatting errors.

--- string_1_solutions.py
+++ string_1_solutions.py
@@ -1,15 +1,17 @@
-# "'-----------------Hello Name-----------------
+# -----------------Hello Name-----------------
 
 # Given a string name, return a greeting of the form "Hello (name)!".
 
 # -------Solution------
 def hello_name(name):
-    return ("Hello {}!". format(name))
+    return "Hello {}!".format(name)
+
 
 # ---------Make_abba-------
 
 # #Given two strings, a and b, return the result of putting them in the order
 # #abba
+
 
 def make_abba(a, b):
     return a + b + b + a
@@ -19,5 +21,6 @@ def make_abba(a, b):
 
 # Given tag and word strings, create the HTML string with tags around the word
 
+
 def make_tags(tag, word):
-    return("<{}>{}</{}>".format(tag, word, tag))
+    return "<{}>{}</{}>".format(tag, word, tag)
